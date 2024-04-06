0f2bdba38be7b5759008a20d5eba641f3adddcfb
Beryl Atieno <berylatieno30@gmail.com>
2024-04-06 13:43:44 +0300

    change Column to Mapped[] approach (sqlalchemy 2.0)

--- python_app/classes.py
+++ python_app/classes.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm import DeclarativeBase, relationship
-from sqlalchemy import Column, Integer, String, BigInteger, SmallInteger, Date, TIMESTAMP
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, BigInteger, SmallInteger, Date, TIMESTAMP
 from sqlalchemy import ForeignKey, PrimaryKeyConstraint, ForeignKeyConstraint
+
+import datetime
 
 
 class Base(DeclarativeBase):
@@ -9,8 +11,8 @@ class Base(DeclarativeBase):
 class Place(Base):
     __tablename__ = 'place'
     
-    placeID = Column(Integer, primary_key=True)
-    name = Column(String(255))
+    placeID:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(55), nullable=False)
 
     __mapper_args__ = {
         'polymorphic_identity': 'place',
@@ -21,7 +23,7 @@ class Place(Base):
 class Continent(Place):
     __tablename__ = 'continent'
     
-    continentID = Column(Integer, ForeignKey('place.placeID'), primary_key=True)
+    continentID:Mapped[int] = mapped_column(ForeignKey('place.placeID'), primary_key=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "continentID",
@@ -32,8 +34,8 @@ class Continent(Place):
 class Country(Place):
     __tablename__ = 'country'
     
-    countryID = Column(Integer, ForeignKey('place.placeID'), primary_key=True)
-    isPartOf = Column(Integer, ForeignKey('continent.continentID'))
+    countryID:Mapped[int] = mapped_column(ForeignKey('place.placeID'), primary_key=True)
+    isPartOf:Mapped[int] = mapped_column(ForeignKey('continent.continentID'))
 
     __mapper_args__ = {
         "polymorphic_identity": "countryID",
@@ -46,8 +48,8 @@ class Country(Place):
 class City(Place):
     __tablename__ = 'city'
     
-    cityID = Column(Integer, ForeignKey('place.placeID'), primary_key=True)
-    isPartOf = Column(Integer, ForeignKey('country.countryID'))
+    cityID:Mapped[int] = mapped_column(ForeignKey('place.placeID'), primary_key=True)
+    isPartOf:Mapped[int] = mapped_column(ForeignKey('country.countryID'))
 
     __mapper_args__ = {
         "polymorphic_identity": "cityID",
@@ -60,15 +62,15 @@ class City(Place):
 class Person(Base):
     __tablename__ = 'person'
     
-    personID = Column(BigInteger, primary_key=True)
-    firstName = Column(String(25), nullable=False)
-    lastName = Column(String(25), nullable=False)
-    gender = Column(String(25))
-    birthday = Column(Date, nullable=False)
-    creationDate = Column(TIMESTAMP)
-    locationIP = Column(String(255))
-    browserUsed = Column(String(255))
-    cityID = Column(Integer, ForeignKey('city.cityID'))
+    personID:Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    firstName:Mapped[str] = mapped_column(String(25), nullable=False)
+    lastName :Mapped[str]= mapped_column(String(25), nullable=False)
+    gender:Mapped[str] = mapped_column(String(25))
+    birthday:Mapped[datetime.datetime] = mapped_column(nullable=False)
+    creationDate = mapped_column(TIMESTAMP)
+    locationIP:Mapped[str] = mapped_column(String(255))
+    browserUsed:Mapped[str] = mapped_column(String(255))
+    cityID:Mapped[int] = mapped_column(Integer, ForeignKey('city.cityID'))
     
     city = relationship("City")
 
@@ -84,40 +86,40 @@ class Person(Base):
 class PersonEmail(Base):
     __tablename__ = 'personemail'
     
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    email = Column(String(255), nullable=False, unique=True)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    email:Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
 
     person = relationship("Person", back_populates="emails")
 
 class Language(Base):
     __tablename__ = 'language'
     
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    language = Column(String(255), nullable=False)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    language:Mapped[str] = mapped_column(String(255), nullable=False)
 
     person = relationship("Person", back_populates="languages")
 
 class Forum(Base):
     __tablename__ = 'forum'
     
-    forumID = Column(BigInteger, primary_key=True)
-    title = Column(String(55))
-    creationDate = Column(TIMESTAMP)
-    moderatorID = Column(BigInteger, ForeignKey('person.personID'))
+    forumID:Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    title:Mapped[str] = mapped_column(String(55))
+    creationDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
+    moderatorID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'))
 
     moderator = relationship("Person", back_populates="forums")
 
 class Message(Base):
     __tablename__ = 'message'
     
-    messageID = Column(BigInteger, primary_key=True)
-    content = Column(String(255))
-    length = Column(Integer)
-    creator = Column(BigInteger, ForeignKey('person.personID'))
-    countryID = Column(BigInteger, ForeignKey('country.countryID'))
-    creationDate = Column(TIMESTAMP)
-    browserused = Column(String(55))
-    locationIP = Column(String(55))
+    messageID:Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    content = mapped_column(String(255))
+    length:Mapped[int] = mapped_column(Integer)
+    creator:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'))
+    countryID:Mapped[int] = mapped_column(BigInteger, ForeignKey('country.countryID'))
+    creationDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
+    browserused:Mapped[str] = mapped_column(String(55))
+    locationIP:Mapped[str] = mapped_column(String(55))
 
     __mapper_args__ = {
         'polymorphic_identity': 'message',
@@ -129,9 +131,9 @@ class Message(Base):
 class Post(Message):
     __tablename__ = 'post'
 
-    postID = Column(BigInteger, ForeignKey('message.messageID'), primary_key=True)
-    imageFile = Column(String(255))
-    forumID = Column(BigInteger, ForeignKey('forum.forumID'))
+    postID:Mapped[int] = mapped_column(BigInteger, ForeignKey('message.messageID'), primary_key=True)
+    imageFile:Mapped[str] = mapped_column(String(255))
+    forumID:Mapped[int] = mapped_column(BigInteger, ForeignKey('forum.forumID'))
 
     __mapper_args__ = {
         'polymorphic_identity': 'postID',
@@ -141,36 +143,36 @@ class Post(Message):
 class Comment(Base):
     __tablename__ = 'comment'
 
-    commentID = Column(BigInteger, primary_key=True)
-    creationDate = Column(TIMESTAMP)
-    locationIP = Column(String(50))
-    browserUsed = Column(String(50))
-    content = Column(String(255))
-    length = Column(Integer)
-    creator = Column(BigInteger)
-    replyOfPost = Column(String(50))
-    replyOfComment = Column(String(50))
+    commentID:Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    creationDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
+    locationIP:Mapped[str] = mapped_column(String(50))
+    browserUsed:Mapped[str] = mapped_column(String(50))
+    content:Mapped[str] = mapped_column(String(255))
+    length:Mapped[int] = mapped_column(Integer)
+    creator:Mapped[int] = mapped_column(BigInteger)
+    replyOfPost:Mapped[str] = mapped_column(String(50))
+    replyOfComment:Mapped[str] = mapped_column(String(50))
 
 class tag(Base):
     __tablename__ = 'tag'
 
-    tagID = Column(BigInteger, primary_key=True)
-    name = Column(String(255), nullable=False)
-    url = Column(String(2048))
+    tagID:Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    name:Mapped[str] = mapped_column(String(255), nullable=False)
+    url:Mapped[str] = mapped_column(String(2048))
 
 class tagClass(Base):
     __tablename__ = 'tagclass'
 
-    tagClassID = Column(Integer, primary_key=True)
-    tagClassName = Column(String(255))
-    url = Column(String(2048))
+    tagClassID:Mapped[int]= mapped_column(Integer, primary_key=True)
+    tagClassName:Mapped[str] = mapped_column(String(255))
+    url:Mapped[str] = mapped_column(String(2048))
 
 
 class tagClass_isSubclassof(Base):
     __tablename__ = 'tagClass_isSubclassof'
 
-    TagClassID_A = Column(Integer)
-    TagClassID_B = Column(Integer)
+    TagClassID_A:Mapped[int] = mapped_column(Integer)
+    TagClassID_B:Mapped[int] = mapped_column(Integer)
 
     __table_args__ = (
         PrimaryKeyConstraint(TagClassID_A, TagClassID_B),
@@ -194,8 +196,8 @@ class tagClass_isSubclassof(Base):
 class Message_hasTag(Base):
     __tablename__ = 'message_hasTag'
 
-    messageID = Column(BigInteger, ForeignKey('message.messageID'), primary_key=True)
-    tagID = Column(Integer, ForeignKey('tag.tagID'), primary_key=True)
+    messageID:Mapped[int] = mapped_column(BigInteger, ForeignKey('message.messageID'), primary_key=True)
+    tagID:Mapped[int] = mapped_column(Integer, ForeignKey('tag.tagID'), primary_key=True)
 
     message = relationship("Message", backref="tags")
     tag = relationship("tag", backref="messages")
@@ -203,8 +205,8 @@ class Message_hasTag(Base):
 class Forum_hasTag(Base):
     __tablename__ = 'forum_hasTag'
 
-    forumID = Column(BigInteger, ForeignKey('forum.forumID'), primary_key=True)
-    tagID = Column(Integer, ForeignKey('tag.tagID'), primary_key=True)
+    forumID:Mapped[int] = mapped_column(BigInteger, ForeignKey('forum.forumID'), primary_key=True)
+    tagID:Mapped[int] = mapped_column(Integer, ForeignKey('tag.tagID'), primary_key=True)
 
     forum = relationship("Forum", backref="tags")
     tag = relationship("tag", backref="forums")
@@ -212,9 +214,9 @@ class Forum_hasTag(Base):
 class Forum_hasMember(Base):
     __tablename__ = 'forum_hasMember'
 
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    forumID = Column(BigInteger, ForeignKey('forum.forumID'), primary_key=True)
-    joinDate = Column(TIMESTAMP)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    forumID:Mapped[int] = mapped_column(BigInteger, ForeignKey('forum.forumID'), primary_key=True)
+    joinDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
 
     person = relationship("Person", backref="forums_joined")
     forum = relationship("Forum", backref="members")
@@ -222,9 +224,9 @@ class Forum_hasMember(Base):
 class Person_likes_Message(Base):
     __tablename__ = 'person_likes_Message'
 
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    messageID = Column(BigInteger, ForeignKey('message.messageID'), primary_key=True)
-    creationDate = Column(TIMESTAMP)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    messageID:Mapped[int] = mapped_column(BigInteger, ForeignKey('message.messageID'), primary_key=True)
+    creationDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
 
     person = relationship("Person", back_populates="likes_messages")
     message = relationship("Message", back_populates="likes")
@@ -232,9 +234,9 @@ class Person_likes_Message(Base):
 class Person_likes_Comment(Base):
     __tablename__ = 'person_likes_Comment'
 
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    commentID = Column(BigInteger, ForeignKey('comment.commentID'), primary_key=True)
-    creationDate = Column(TIMESTAMP)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    commentID:Mapped[int] = mapped_column(BigInteger, ForeignKey('comment.commentID'), primary_key=True)
+    creationDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
 
     person = relationship("Person", back_populates="likes_comments")
     comment = relationship("Comment", backref="likes")
@@ -242,9 +244,9 @@ class Person_likes_Comment(Base):
 class Person_knows_Person(Base):
     __tablename__ = 'person_knows_Person'
 
-    personID_A = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    personID_B = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    creationDate = Column(TIMESTAMP)
+    personID_A:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    personID_B:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    creationDate:Mapped[datetime.datetime] = mapped_column(TIMESTAMP)
 
     person_A = relationship("Person", foreign_keys=[personID_A])
     person_B = relationship("Person", foreign_keys=[personID_B])
@@ -252,8 +254,8 @@ class Person_knows_Person(Base):
 class Person_hasinterest(Base):
     __tablename__ = 'person_hasinterest'
 
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    tagID = Column(Integer, ForeignKey('tag.tagID'), primary_key=True)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    tagID:Mapped[int] = mapped_column(Integer, ForeignKey('tag.tagID'), primary_key=True)
 
     person = relationship("Person", backref="interests")
     tag = relationship("tag", backref="people_interested")
@@ -261,9 +263,9 @@ class Person_hasinterest(Base):
 class Organisation(Base):
     __tablename__ = 'organisation'
     
-    organisationID = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    url = Column(String(2048))
+    organisationID:Mapped[int] = mapped_column(Integer, primary_key=True)
+    name:Mapped[str] = mapped_column(String(255), nullable=False)
+    url:Mapped[str] = mapped_column(String(2048))
 
     __mapper_args__ = {
         'polymorphic_identity': 'organisation',
@@ -273,8 +275,8 @@ class Organisation(Base):
 class Company(Organisation):
     __tablename__ = 'company'
 
-    companyID = Column(Integer, ForeignKey('organisation.organisationID'), primary_key=True)
-    countryID = Column(Integer, ForeignKey('country.countryID'))
+    companyID:Mapped[int] = mapped_column(Integer, ForeignKey('organisation.organisationID'), primary_key=True)
+    countryID:Mapped[int] = mapped_column(Integer, ForeignKey('country.countryID'))
 
     __mapper_args__ = {
         "polymorphic_identity": "companyID",
@@ -285,8 +287,8 @@ class Company(Organisation):
 class University(Organisation):
     __tablename__ = 'university'
 
-    universityID = Column(Integer, ForeignKey('organisation.organisationID'), primary_key=True)
-    cityID = Column(Integer, ForeignKey('city.cityID'))
+    universityID:Mapped[int] = mapped_column(Integer, ForeignKey('organisation.organisationID'), primary_key=True)
+    cityID:Mapped[int] = mapped_column(Integer, ForeignKey('city.cityID'))
     
     __mapper_args__ = {
         "polymorphic_identity": "universityID",
@@ -297,9 +299,9 @@ class University(Organisation):
 class StudyAt(Base):
     __tablename__ = 'studyAT'
 
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    universityID = Column(Integer, ForeignKey('university.universityID'), primary_key=True)
-    classYear = Column(SmallInteger, nullable=False)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    universityID:Mapped[int] = mapped_column(Integer, ForeignKey('university.universityID'), primary_key=True)
+    classYear:Mapped[int] = mapped_column(SmallInteger, nullable=False)
 
     person = relationship("Person", backref="studies")
     university = relationship("University", backref="students")
@@ -307,9 +309,9 @@ class StudyAt(Base):
 class WorkAt(Base):
     __tablename__ = 'workAT'
 
-    personID = Column(BigInteger, ForeignKey('person.personID'), primary_key=True)
-    companyID = Column(Integer, ForeignKey('company.companyID'), primary_key=True)
-    workFrom = Column(SmallInteger, nullable=False)
+    personID:Mapped[int] = mapped_column(BigInteger, ForeignKey('person.personID'), primary_key=True)
+    companyID:Mapped[int] = mapped_column(Integer, ForeignKey('company.companyID'), primary_key=True)
+    workFrom:Mapped[int] = mapped_column(SmallInteger, nullable=False)
 
     person = relationship("Person", backref="works")
     company = relationship("Company", backref="employees")
