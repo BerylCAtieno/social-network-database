41efa195f3a675fecc41a712a19d2e0a6a0326ff
Beryl Atieno <berylatieno30@gmail.com>
2024-05-01 10:12:40 +0300

    edit database params

--- python_app/create_engine.py
+++ python_app/create_engine.py
@@ -6,15 +6,15 @@ password = 'password'
 hostname = 'localhost'
 port = '5432'  
 database_name = 'social_2'
-schema_name = 'social_network_db'
+dbschema = 'social_network_db'
 
 # Create db URL
 
-database_url = f"postgresql://{username}:{password}@{hostname}:{port}/{database_name}?currentSchema={schema_name}"
+database_url = f"postgresql://{username}:{password}@{hostname}:{port}/{database_name}"
 
 
 #create engine object
-engine = create_engine(database_url)
+engine = create_engine(database_url, connect_args={'options': '-csearch_path={}'.format(dbschema)})
 
 
 
